18d167665513a5f1f684a9764fb55530d3ff5f39
saward <mark.saward@episub.com>
2019-04-16 22:18:38 +1000

    improved db templates and documentation for user authentication option

--- cmd/static/gnorm/where.go
+++ cmd/static/gnorm/where.go
@@ -3,6 +3,8 @@ package gnorm
 import (
 	"fmt"
 	"strings"
+
+	"github.com/gofrs/uuid"
 )
 
 // InString Returns an in clause for an array of strings
@@ -31,6 +33,17 @@ func InInt(field string, values []int) In {
 type In struct {
 	Field  string
 	Values []interface{}
+}
+
+// InUUIDUUID Returns an in clause for an array of uuid's
+func InUUIDUUID(field string, values []uuid.UUID) In {
+	newVals := make([]interface{}, len(values))
+
+	for i, v := range values {
+		newVals[i] = v
+	}
+
+	return In{Field: field, Values: newVals}
 }
 
 // ToSql Returns the sql related objects expected by squirrel
