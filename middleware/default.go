abc9f44776deb3d2b9759cffa815deac5295e3cf
saward <mark.saward@episub.com>
2019-04-16 22:18:44 +1000

    improved db templates and documentation for user authentication option

---
+++ middleware/default.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/episub/estack/store"
+	"github.com/episub/estack/validate"
+)
+
+// SharedData Name of value in context that holds OPA related data
+const SharedData = "defaultSharedData"
+
+// DefaultMW Sets up items needed for most requests
+// - Adds a data object to the context, used for passing data through to OPA requests
+// - Sets validation context
+func DefaultMW(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), SharedData, store.NewDataStore())
+		ctx = validate.SetContext(r.Context())
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
